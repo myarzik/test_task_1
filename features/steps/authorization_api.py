b55e96c2e3ed6f5e6ba7a41019e2e2ef00354727
myarzik <myarzik@gmail.com>
2018-04-18 18:22:49 +0500

    Update authorization_api.py

--- features/steps/authorization_api.py
+++ features/steps/authorization_api.py
@@ -1,1 +1,14 @@
+import behave
+import requests
 
+@given('')
+def step_impl(context):
+	pass
+	
+@when('send default authorization request')
+def step_impl(context):
+	assert True is not False
+	
+@then('back-end response is ok')
+def step_impl(context):
+	assert context.failed is False
